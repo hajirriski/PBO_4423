6a9aafac123f48ab62c17785abb4d0a046d66d3e
hajirriski <72801049+hajirriski@users.noreply.github.com>
2021-06-04 21:22:52 +0700

    Create Main.java

---
+++ prak11/src/kendaraan/Main.java
@@ -0,0 +1,10 @@
+package kendaraan;
+
+public class Main {
+    public static void main(String[] args) {
+        Sepeda sepedaJenis1 = new Sepeda(true);
+        sepedaJenis1.jenis();
+        Sepeda sepedaJenis2 = new Sepeda(false);
+        sepedaJenis2.jenis();
+    }
+}
